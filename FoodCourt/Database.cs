c2b9b7ad8fb3dbd01742bbfda877de381abdb200
NazninNaharSr <nazninns58@gmail.com>
2022-04-03 21:29:06 +0600

    Add project files.

---
+++ FoodCourt/Database.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodCourt
+{
+    class Database
+    {
+        public static SqlConnection ConnectDB()
+        {
+            string connString = @"Server=RABBY-PC\SQLEXPRESS;Database=FoodCourt;User Id=sa;Password=1234;";
+            SqlConnection conn = new SqlConnection(connString);
+            return conn;
+        }
+    }
+}
